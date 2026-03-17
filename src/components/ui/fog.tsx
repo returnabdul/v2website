b67d8790f05806887a51767b59a686900e304baa
returnabdul <abdulgame47@gmail.com>
2026-03-17 13:01:25 +0000

    feat: add light mode and dark mode fog
    - the fog background shall change the fog color
      based on theme

--- src/components/ui/fog.tsx
+++ src/components/ui/fog.tsx
@@ -4,31 +4,52 @@ import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 // @ts-ignore
 import FOG from "vanta/dist/vanta.fog.min";
+import { useTheme } from "next-themes";
 
 export function VantaFog({ children }: { children?: React.ReactNode }) {
   const vantaRef = useRef<HTMLDivElement>(null);
   const [vantaEffect, setVantaEffect] = useState<any>(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        FOG({
-          el: vantaRef.current,
-          THREE: THREE,
-          highlightColor: 0x7b2ff7,
-          midtoneColor:   0xc084fc,
-          lowlightColor:  0xf5f3ff,
-          baseColor:      0xffffff,
-          blurFactor:     0.6,
-          zoom:           1,
-          speed:          1,
-        })
-      );
+    if (vantaEffect) {
+      vantaEffect.destroy();
     }
+
+    const isDark = theme === "dark";
+
+    const effect = FOG({
+      el: vantaRef.current,
+      THREE: THREE,
+
+      // dark mode colors
+      ...(isDark
+        ? {
+            // TODO: determine colors
+            highlightColor: 0xffc300,
+            midtoneColor: 0x341e1b,
+            lowlightColor: 0x172c93,
+            baseColor: 0x163669,
+          }
+        : {
+            // light mode colors
+            highlightColor: 0x7b2ff7,
+            midtoneColor: 0xc084fc,
+            lowlightColor: 0xf5f3ff,
+            baseColor: 0xffffff,
+          }),
+
+      blurFactor: 0.8,
+      zoom: 0.5,
+      speed: 0.3,
+    });
+
+    setVantaEffect(effect);
+
     return () => {
-      if (vantaEffect) vantaEffect?.destroy();
+      effect.destroy();
     };
-  }, [vantaEffect?.current]);
+  }, [theme]);
 
   return (
     <div ref={vantaRef} className="w-full h-full">
